ea933a631c98fe364636cd2aea3b07236fd10090
Jeremiakim <jeremikimzu67@gmail.com>
2023-12-08 20:38:06 +0700

    feat : make middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,42 +1,42 @@
-// import { cookies, headers } from "next/headers";
-// import { NextRequest, NextResponse } from "next/server";
-// import { readPayloadJose } from "./lib/jwt";
+import { cookies, headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { readPayloadJose } from "./lib/jwt";
 
-// export const middleware = async (req: NextRequest) => {
-//   if (
-//     !req.url.includes("/api") &&
-//     !req.url.includes("_next/static") &&
-//     !req.url.includes("_next/image") &&
-//     !req.url.includes("favicon.ico")
-//   ) {
-//     console.log(req.method, req.url);
-//   }
+export const middleware = async (req: NextRequest) => {
+  if (
+    !req.url.includes("/api") &&
+    !req.url.includes("_next/static") &&
+    !req.url.includes("_next/image") &&
+    !req.url.includes("favicon.ico")
+  ) {
+    console.log(req.method, req.url);
+  }
 
-//   if (req.url.includes("/apis")) {
-//     console.log("apis", req.url, "hello baru nii");
-//     const cookiesStore = cookies();
+  if (req.url.includes("/apis")) {
+    console.log("apis", req.url, "hello baru nii");
+    const cookiesStore = cookies();
 
-//     const token = cookiesStore.get("token");
-//     console.log(token);
+    const token = cookiesStore.get("token");
+    console.log(token);
 
-//     if (!token) {
-//       return NextResponse.json({
-//         statusCode: 401,
-//         error: "Unauthorized",
-//       });
-//     }
+    if (!token) {
+      return NextResponse.json({
+        statusCode: 401,
+        error: "Unauthorized",
+      });
+    }
 
-//     const tokenData = await readPayloadJose<{ id: string; email: string }>(
-//       token.value
-//     );
+    const tokenData = await readPayloadJose<{ id: string; email: string }>(
+      token.value
+    );
 
-//     const requestHeaders = new Headers(req.headers);
-//     requestHeaders.set("x-user-id", tokenData.id);
-//     requestHeaders.set("x-user-email", tokenData.email);
+    const requestHeaders = new Headers(req.headers);
+    requestHeaders.set("x-user-id", tokenData.id);
+    requestHeaders.set("x-user-email", tokenData.email);
 
-//     return NextResponse.json({
-//       headers: requestHeaders,
-//     });
-//   }
-//   return NextResponse.next();
-// };
+    return NextResponse.json({
+      headers: requestHeaders,
+    });
+  }
+  return NextResponse.next();
+};
